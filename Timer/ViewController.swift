ccb60c28a095fc46b8bc0b2236b6d9f8966d38ea
Rentaro Suzuki <rentajaxa96@gmail.com>
2024-11-09 20:35:05 +0900

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by 鈴木廉太郎 on 2024/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var timer: Timer!
+    
+    var countdown: Int = 0
+    
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func select30seconds() {
+        
+        countdown = 30
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerCalled), userInfo: nil, repeats: true)
+        
+    }
+
+}
+
