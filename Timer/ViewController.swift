f53f658962c2c94144d84c6671982952c9fe2145
Rentaro Suzuki <rentajaxa96@gmail.com>
2024-11-23 15:16:17 +0900

    エラーが発生しました。

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -19,11 +19,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @objc func onTimercalled() {
+        
+        let remainingMinutes: Int = countdown / 60
+        let remainingSeconds: Int = countdown % 60
+        
+        
+        label.text = String(format: "%02d: %02d" ,remainingMinutes, remainingSeconds)
+                            countdown -= 1
+        
+        if countdown < 0 {
+            
+            let stopAlert = UIAlertController(title: "タイマーが終了しました",message:nil,preferredStyle: .alert )
+            let okAction = UIAlertAction(title: "OK" , style: .default)
+            
+            stopAlert .addAction(okAction)
+            
+            present(stopAlert, animated: true)
+            
+            timer.invalidate()
+        }
+        
+        
+    }
 
     @IBAction func select30seconds() {
         
         countdown = 30
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerCalled), userInfo: nil, repeats: true)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:   #selector(onTimercalled()), userInfo: nil, repeats: true)
+        
+        
+        
+        timer.fire() //タイマーを開始
+        
         
     }
 
